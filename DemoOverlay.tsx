ba161b7ea3c329ea6073ea9a00bcb5e04eeec16c
M. Vagnon <matthieu.vagnon@predictivelayer.com>
2024-10-28 19:16:54 +0100

    Updating design for a more minimalist one

--- DemoOverlay.tsx
+++ DemoOverlay.tsx
@@ -22,7 +22,7 @@ export default function DemoOverlay(props: DemoOverlayProps) {
         <div className="back-button-container">
           <a href={backButton.url} className="back-button">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-              <path d="M526-314 381-459q-5-5-7-10t-2-11q0-6 2-11t7-10l145-145q3-3 6.5-4.5t7.5-1.5q8 0 14 5.5t6 14.5v304q0 9-6 14.5t-14 5.5q-2 0-14-6Z" />
+              <path d="M534-306 376-464q-3-3-5-7.06-2-4.07-2-8.94 0-4.88 2-8.94t5-7.06l158-158q2-2 4.76-3.5 2.77-1.5 5.92-1.5 6.32 0 10.82 4.12 4.5 4.13 4.5 10.88v328q0 6.75-4.64 10.87-4.63 4.13-10.81 4.13-1.55 0-10.55-5Z" />
             </svg>
             <span>{backButton.title}</span>
           </a>
