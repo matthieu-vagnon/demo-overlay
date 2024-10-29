5aa74c6826d721a1f616982511a9f3717e2ba225
M. Vagnon <matthieu.vagnon@predictivelayer.com>
2024-10-29 08:57:28 +0100

    Updating UI

--- DemoOverlay.tsx
+++ DemoOverlay.tsx
@@ -5,7 +5,7 @@ import "./DemoOverlay.css";
 
 // #region Configuration
 const backButton = {
-  title: "Portfolio",
+  title: "mvagnon.dev",
   url: "https://www.mvagnon.dev",
 };
 // #endregion
@@ -63,11 +63,11 @@ export default function DemoOverlay(props: DemoOverlayProps) {
           <div className="menu-container">
             <a href={backButton.url} className="back-link">
               <svg
-                height="85%"
+                height="20%"
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 -960 960 960"
+                viewBox="0 0 122.88 108.06"
               >
-                <path d="M534-306 376-464q-3-3-5-7.06-2-4.07-2-8.94 0-4.88 2-8.94t5-7.06l158-158q2-2 4.76-3.5 2.77-1.5 5.92-1.5 6.32 0 10.82 4.12 4.5 4.13 4.5 10.88v328q0 6.75-4.64 10.87-4.63 4.13-10.81 4.13-1.55 0-10.55-5Z" />
+                <path d="M63.94,24.28a14.28,14.28,0,0,0-20.36-20L4.1,44.42a14.27,14.27,0,0,0,0,20l38.69,39.35a14.27,14.27,0,0,0,20.35-20L48.06,68.41l60.66-.29a14.27,14.27,0,1,0-.23-28.54l-59.85.28,15.3-15.58Z" />
               </svg>
               <span>{backButton.title}</span>
             </a>
