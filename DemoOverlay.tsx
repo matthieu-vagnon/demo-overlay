bb1ae7ffb0ec509b8cd457de2ac0f0f63beea7cd
M. Vagnon <matthieu.vagnon@predictivelayer.com>
2024-11-03 18:30:13 +0100

    Changing base icon

--- DemoOverlay.tsx
+++ DemoOverlay.tsx
@@ -37,12 +37,13 @@ export default function DemoOverlay(props: DemoOverlayProps) {
         <button className="demo-overlay-switch" onClick={handleSwitch}>
           <div className="demo-overlay-switch-icon-container">
             <div className="demo-overlay-switch-icon demo-overlay-open-icon">
-              <img
-                height="25%"
-                width="25%"
-                src={`${backButton.url}/logo-mono-white.svg`}
-                alt="Portfolio Logo"
-              />
+              <svg
+                height="50%"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 -960 960 960"
+              >
+                <path d="m313-440 196 196q12 12 11.5 28T508-188q-12 11-28 11.5T452-188L188-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l264-264q11-11 27.5-11t28.5 11q12 12 12 28.5T508-715L313-520h447q17 0 28.5 11.5T800-480q0 17-11.5 28.5T760-440H313Z" />
+              </svg>
             </div>
             <div className="demo-overlay-switch-icon demo-overlay-close-icon">
               <svg
